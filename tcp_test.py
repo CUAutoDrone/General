e54b93b741bffec8864a7a995dcaa25756837795
Nikhil <Ranganathan@dhcp-uris-2994.redrover.cornell.edu>
2018-10-12 15:41:49 -0400

    tcp test

---
+++ tcp_test.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = '192.168.2.2'
+PORT = 9989
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind((HOST,PORT))
+	s.listen()
+	conn,addr = s.accept()
+	with conn:
+		print('Connected by',addr)
+		while True:
+			conn.sendall(b'3')
+		
