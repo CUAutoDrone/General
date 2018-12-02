37464bc4118018b22d79f8f41743fbff7ef2bf06
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2018-12-02 14:02:30 -0500

    updated constructor
    IMU, receiver, motors initialized with flight controller object

--- FlightController_2.py
+++ FlightController_2.py
@@ -190,7 +190,7 @@ class Motor(object):
         self.MOTOR3 = MOTOR3
         self.MOTOR4 = MOTOR4
 
-    def map_motor_output(ctrl):
+    def map_motor_output(self, ctrl):
         throttle = ctrl[0]
         roll = ctrl[1]
         pitch = ctrl[2]
@@ -202,7 +202,7 @@ class Motor(object):
         m4 = throttle + roll + pitch + yawrate
         return Motor.minMax(np.array([m1, m2, m3, m4]), 1, 2)
 
-    def minMax(vec, min, max):
+    def minMax(self, vec, min, max):
         for i in range(0, np.size(vec)):
             if vec[i] < min:
                 vec[i] = min
@@ -210,7 +210,7 @@ class Motor(object):
                 vec[i] = max
         return vec
 
-    def set_motor_pulse(pi, gpio, timems):
+    def set_motor_pulse(self, pi, gpio, timems):
         pi.set_PWM_dutycycle(gpio, timems / 2.5 * 255)
 
     def arm(self, pi):
