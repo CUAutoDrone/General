2912d4e450849a0fd43b4d810543fc774736997e
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-01-06 10:52:06 -0500

    Update Receiver.py
    - Removed global labels from PWM measurements, initialized as local variables.
    - Updated and renamed can_arm() method to throttle_safe() method

--- flight_controller/Receiver.py
+++ flight_controller/Receiver.py
@@ -11,137 +11,111 @@ class Receiver(object):
         self.RECEIVER_CH5 = CH5
         self.ARM = False
 
-    # GLOBAL VARIABLES FOR PWM MEASUREMENT
-    rising_1 = 0
-    pulse_width_ch1 = 0
-    rising_2 = 0
-    pulse_width_ch2 = 0
-    rising_3 = 0
-    pulse_width_ch3 = 0
-    rising_4 = 0
-    pulse_width_ch4 = 0
-    rising_5 = 0
-    pulse_width_ch5 = 0
+        # VARIABLES FOR PWM MEASUREMENT
+        self.rising_1 = 0
+        self.pulse_width_ch1 = 0
+        self.rising_2 = 0
+        self.pulse_width_ch2 = 0
+        self.rising_3 = 0
+        self.pulse_width_ch3 = 0
+        self.rising_4 = 0
+        self.pulse_width_ch4 = 0
+        self.rising_5 = 0
+        self.pulse_width_ch5 = 0
 
-    @staticmethod
-    def cbf1(gpio, level, tick):
-        # connects global and local variables
-        global rising_1
-        global pulse_width_ch1
+    def cbf1(self, gpio, level, tick):
 
         # If rising edge, store time
         if level == 1:
-            rising_1 = tick
+            self.rising_1 = tick
         # If falling edge, subtract out rising time to get pulse width
         elif level == 0:
 
-            width = tick - rising_1
+            width = tick - self.rising_1
             # in case of wraparound
             if width > 0:
                 # divide by 1000 to get milliseconds
-                pulse_width_ch1 = width / 1000
+                self.pulse_width_ch1 = width / 1000
 
-    @staticmethod
-    def cbf2(gpio, level, tick):
-        # connects global and local variables
-        global rising_2
-        global pulse_width_ch2
+    def cbf2(self, gpio, level, tick):
 
         # If rising edge, store time
         if level == 1:
-            rising_2 = tick
+            self.rising_2 = tick
         # If falling edge, subtract out rising time to get pulse width
         elif level == 0:
 
-            width = tick - rising_2
+            width = tick - self.rising_2
             # in case of wraparound
             if width > 0:
                 # divide by 1000 to get milliseconds
-                pulse_width_ch2 = width / 1000
+                self.pulse_width_ch2 = width / 1000
 
-    @staticmethod
-    def cbf3(gpio, level, tick):
-        # connects global and local variables
-        global rising_3
-        global pulse_width_ch3
+    def cbf3(self, gpio, level, tick):
 
         # If rising edge, store time
         if level == 1:
-            rising_3 = tick
+            self.rising_3 = tick
         # If falling edge, subtract out rising time to get pulse width
         elif level == 0:
 
-            width = tick - rising_3
+            width = tick - self.rising_3
             # in case of wraparound
             if width > 0:
                 # divide by 1000 to get milliseconds
-                pulse_width_ch3 = width / 1000
+                self.pulse_width_ch3 = width / 1000
 
-    @staticmethod
-    def cbf4(gpio, level, tick):
-        # connects global and local variables
-        global rising_4
-        global pulse_width_ch4
+    def cbf4(self, gpio, level, tick):
 
         # If rising edge, store time
         if level == 1:
-            rising_4 = tick
+            self.rising_4 = tick
         # If falling edge, subtract out rising time to get pulse width
         elif level == 0:
 
-            width = tick - rising_4
+            width = tick - self.rising_4
             # in case of wraparound
             if width > 0:
                 # divide by 1000 to get milliseconds
-                pulse_width_ch4 = width / 1000
+                self.pulse_width_ch4 = width / 1000
 
     def cbf5(self, gpio, level, tick):
-        # connects global and local variables
-        global rising_5
-        global pulse_width_ch5
 
         # If rising edge, store time
         if level == 1:
-            rising_5 = tick
+            self.rising_5 = tick
         # If falling edge, subtract out rising time to get pulse width
         elif level == 0:
 
-            width = tick - rising_5
+            width = tick - self.rising_5
             # in case of wraparound
             if width > 0:
                 # divide by 1000 to get milliseconds
-                pulse_width_ch5 = width / 1000
-                if pulse_width_ch5 > 1.4:
-                    self.receiver.ARM = True
+                self.pulse_width_ch5 = width / 1000
+                if self.pulse_width_ch5 > 1.4:
+                    self.ARM = True
                 else:
-                    self.receiver.ARM = False
+                    self.ARM = False
 
     @staticmethod
     def map(num, a, b, c, d):
         y = (num - a) * (d - c) / (b - a) + c
         return y
 
-    @staticmethod
-    def map_control_input():
-        global pulse_width_ch1
-        global pulse_width_ch2
-        global pulse_width_ch3
-        global pulse_width_ch4
-        global pulse_width_ch5
+    def map_control_input(self):
 
-        roll = Receiver.map(pulse_width_ch2, 1, 2, -30, 30)
-        pitch = Receiver.map(pulse_width_ch4, 1, 2, -30, 30)
-        yaw = Receiver.map(pulse_width_ch1, 1, 2, -10, 10)
-        throttle = pulse_width_ch3
-        arm = pulse_width_ch5
+        roll = Receiver.map(self.pulse_width_ch2, 1, 2, -30, 30)
+        pitch = Receiver.map(self.pulse_width_ch4, 1, 2, -30, 30)
+        yaw = Receiver.map(self.pulse_width_ch1, 1, 2, -10, 10)
+        throttle = self.pulse_width_ch3
+        arm = self.pulse_width_ch5
 
         return np.array([roll, pitch, yaw, throttle, arm])
 
-    @staticmethod
-    def can_arm():
-        if pulse_width_ch3 > 1.0:
-            print("Failed Pre-Flight Check.  Throttle Not Zero")
+    def throttle_safe(self):
+        if self.pulse_width_ch3 > 1.0:
+            print("Throttle is not zero")
             return False
         else:
-            print("Passed arming requirements")
+            print("Throttle is zeroed")
             return True
