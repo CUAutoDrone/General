d6d7f2aaeb672bd8eebd275d70c368ce69eeba8b
Chris Gyurgyik <cpg49@cornell.edu>
2019-03-31 10:53:01 -0400

    test ssh thoughts

--- flight_controller/GUI/test_ssh.py
+++ flight_controller/GUI/test_ssh.py
@@ -5,9 +5,13 @@ from vassal.scheduler import Scheduler
 # https://github.com/Shawn-Shan/vassal
 
 
-# shell = Terminal(["scp username@host:/home/foo.txt foo_local.txt"])
-
 if __name__ == "__main__":
     print("Connecting...")
-    shell = Terminal(["ssh pi@192.168.1.12", "raspberry", "scp 1.txt pi@192.168.1.12:/home/pi/"])
+    shell = Terminal(["ssh pi@192.168.1.12", "scp 1.txt pi@192.168.1.12:/home/pi/"], password="raspberry")
     shell.run()
+
+
+#### Thoughts ####
+# Command to execute script to run the drone
+# In that script, ways to gather data to either Webserver/TCP
+### - Options: write data to .txt file, read .txt file and copy to Webserver
